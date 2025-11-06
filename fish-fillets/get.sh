0f7e280fb0d40f3f006b646c12993f207dd9bcb7
publicsite <34146156+publicsite@users.noreply.github.com>
2025-11-06 19:39:15 +0000

    Update get.sh Set umask

--- fish-fillets/get.sh
+++ fish-fillets/get.sh
@@ -1,7 +1,13 @@
 #!/bin/sh
+
+OLD_UMASK="$(umask)"
+umask 0022
+
 mkdir Download
 cd Download
 
 wget "http://prdownloads.sourceforge.net/fillets/fillets-ng-1.0.1.tar.gz?download" -O fillets-ng-1.0.1.tar.gz
 wget "http://prdownloads.sourceforge.net/fillets/fillets-ng-data-1.0.1.tar.gz?download" -O fillets-ng-data-1.0.1.tar.gz
 wget "https://fillets.sourceforge.net/intro.avi"
+
+umask "${OLD_UMASK}"
