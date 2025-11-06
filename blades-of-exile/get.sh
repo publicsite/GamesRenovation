8fc5440cc13045e834fdbc6d8550fd8e7bcec7f6
publicsite <34146156+publicsite@users.noreply.github.com>
2025-11-06 19:28:37 +0000

    Update get.sh Set umask

--- blades-of-exile/get.sh
+++ blades-of-exile/get.sh
@@ -1,7 +1,12 @@
 #!/bin/sh
+OLD_UMASK="$(umask)"
+umask 0022
+
 mkdir Download
 cd Download
 ##wget "http://www.spiderwebsoftware.com/ftp/mac/bladesofexilesource.zip"
 ##git clone https://github.com/texus/TGUI/ # dependency
 wget https://github.com/texus/TGUI/archive/refs/tags/v0.8.8.tar.gz
 git clone https://github.com/calref/cboe
+
+umask "${OLD_UMASK}"
