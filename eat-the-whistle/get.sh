43f1bab95cd6ebaa2dbdd4696b750a26304cc034
publicsite <34146156+publicsite@users.noreply.github.com>
2025-11-06 19:35:47 +0000

    Update get.sh Set umask

--- eat-the-whistle/get.sh
+++ eat-the-whistle/get.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
+
+OLD_UMASK="$(umask)"
+umask 0022
+
 mkdir Download
 cd Download
 wget "https://sourceforge.net/projects/etw/files/etw-sources/3.6/ETW-3.6-src.tar.gz/download" -O ETW-3.6-src.tar.gz
+
+umask "${OLD_UMASK}"
