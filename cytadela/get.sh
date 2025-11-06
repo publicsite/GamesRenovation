f5baceb5f73bc13a3141507f48b3b275be4af22f
publicsite <34146156+publicsite@users.noreply.github.com>
2025-11-06 19:34:35 +0000

    Update get.sh Set umask

--- cytadela/get.sh
+++ cytadela/get.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
+
+OLD_UMASK="$(umask)"
+umask 0022
+
 mkdir Download
 cd Download
 wget "http://sourceforge.net/projects/cytadela/files/cytadela/1.1.0/cytadela-1.1.0.tar.bz2/download" -O cytadela-1.1.0.tar.bz2
+
+umask "${OLD_UMASK}"
