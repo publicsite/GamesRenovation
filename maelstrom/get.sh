f1ed5d7b859857dce9f2a352d7513b0e837d6c79
publicsite <34146156+publicsite@users.noreply.github.com>
2025-11-07 18:00:45 +0000

    Update get.sh Set umask

--- maelstrom/get.sh
+++ maelstrom/get.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
+
+OLD_UMASK="$(umask)"
+umask 0022
+
 mkdir Download
 cd Download
 
 wget "https://web.archive.org/web/20200721074403/http://hg.libsdl.org/Maelstrom/archive/0e3d2ddd9add.tar.bz2" -O Mealstom-archiveorg-snapshot-21-jul-2020.bz2
+
+umask "${OLD_UMASK}"
