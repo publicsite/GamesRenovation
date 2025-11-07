a583c304dc6e13ebf189a5a66d5ac38940044f5f
publicsite <34146156+publicsite@users.noreply.github.com>
2025-11-07 17:56:16 +0000

    Update get.sh Set umask

--- habitat/get.sh
+++ habitat/get.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
+
+OLD_UMASK="$(umask)"
+umask 0022
+
 mkdir Download
 cd Download
 
 git clone "https://github.com/Museum-of-Art-and-Digital-Entertainment/habitat"
+
+umask "${OLD_UMASK}"
