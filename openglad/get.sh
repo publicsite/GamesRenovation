e9deb0a9786c8761c906508a694f62581daeacf5
publicsite <34146156+publicsite@users.noreply.github.com>
2025-11-08 14:58:01 +0000

    Update get.sh Set umask

--- openglad/get.sh
+++ openglad/get.sh
@@ -1,7 +1,13 @@
 #!/bin/sh
+
+OLD_UMASK="$(umask)"
+umask 0022
+
 mkdir Download
 cd Download
 
 git clone https://github.com/openglad/openglad
 
 #wget "http://prdownloads.sourceforge.net/snowstorm/openglad-0.98.tar.gz?download" -O openglad-0.98.tar.gz
+
+umask "${OLD_UMASK}"
