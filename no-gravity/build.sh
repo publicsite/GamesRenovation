9b37979aaa458801c6b518c2cbe0f635ba1e0c57
publicsite <34146156+publicsite@users.noreply.github.com>
2025-11-08 14:55:58 +0000

    Update build.sh Set umask

--- no-gravity/build.sh
+++ no-gravity/build.sh
@@ -1,7 +1,13 @@
 #!/bin/sh
+
+OLD_UMASK="$(umask)"
+umask 0022
+
 mkdir -p build/data
 mkdir -p build/game
 unzip -d build/game Download/rt-nogravity-src.zip
 unzip -d build/data Download/rt-nogravity-data.zip
 
 cd build
+
+umask "${OLD_UMASK}"
